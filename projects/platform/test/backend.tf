b0613b84fac2c8e72039e9f30efc45d884144182
Khaled Ahmed <khaledahmed@192.168.1.21>
2025-05-09 10:29:47 +0300

    update backend

--- projects/platform/test/backend.tf
+++ projects/platform/test/backend.tf
@@ -16,6 +16,9 @@ terraform {
     region         = "eu-west-1"                   
     encrypt        = true
     use_lockfile   = true
+    assume_role = {
+      role_arn = "arn:aws:iam::767397883134:role/terraform-role"
+    }
   }
 }
 
